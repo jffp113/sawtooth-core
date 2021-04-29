408d9cb27b47c5a9e5fa8ee9d94b18e2911399ba
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2021-04-29 10:41:17 +0100

    Config File: Added support to launch the bootstrap in sawtooth

--- docker/jorge/setup/config/start.sh
+++ docker/jorge/setup/config/start.sh
@@ -3,12 +3,13 @@
 # Purpose: Run multiple sawtooth nodes on multiple machines
 # ----------------------------------------------------
 
-SERVERS=(51.83.75.29 51.83.75.29 51.83.75.29 51.83.75.29)
-SIGNERSNODES=(51.83.75.29 51.83.75.29 51.83.75.29 51.83.75.29)
+SERVERS=(51.83.75.29 51.83.75.29 51.83.75.29 51.83.75.29 51.83.75.29)
+SIGNERSNODES=(51.83.75.29 51.83.75.29 51.83.75.29 51.83.75.29 51.83.75.29)
 
 #Do not change bellow
 ID=1
-HOST_PORT=8800
+SEED_PORT=8800 #Do not change
+HOST_PORT=$SEED_PORT
 API_PORT=8008
 SIGNER_PORT=7000
 
@@ -24,7 +25,20 @@ SIGNER_PORT=7000
 #HOST_PORT=$((HOST_PORT + 1))
 #API_PORT=$((API_PORT + 1))
 
-for index in ${!SERVERS[@]}
+ CMD="bash -c '
+    export ID=${ID} &&
+    export HOST_IP=${s} &&
+    export HOST_PORT=${HOST_PORT} &&
+    export API_PORT=${API_PORT} &&
+    export SIGNERNODE=${SIGNERSNODES[ID - 1]}:${SIGNER_PORT} &&
+    cd /home/jfp/sawtooth-core/docker/jorge/setup/config &&
+    docker-compose -p ${ID} -f sawtooth-peer.yaml up --detach
+    '
+  "
+  echo "- ${SERVERS[ID - 1]}:${API_PORT}"
+  echo $CMD | ssh -t jfp@${SERVERS[ID - 1]} bash
+
+for s in ${SERVERS[@]:1}
 do
   ID=$((ID + 1))
   HOST_PORT=$((HOST_PORT + 1))
@@ -32,15 +46,15 @@ do
   SIGNER_PORT=$((SIGNER_PORT + 1))
   CMD="bash -c '
     export ID=${ID} &&
-    export HOST_IP=${SERVERS[index]} &&
+    export HOST_IP=${s} &&
     export HOST_PORT=${HOST_PORT} &&
     export API_PORT=${API_PORT} &&
-    export SIGNERNODE=${SIGNERSNODES[index]}:${SIGNER_PORT}
-    export SEED=tcp://51.83.75.29:8800 &&
+    export SIGNERNODE=${SIGNERSNODES[ID - 1]}:${SIGNER_PORT}
+    export SEED=tcp://51.83.75.29:${SEED_PORT} &&
     cd /home/jfp/sawtooth-core/docker/jorge/setup/config &&
     docker-compose -p ${ID} -f sawtooth-peer.yaml up --detach
     '
   "
   echo $CMD | ssh -t jfp@${s} bash
-  echo "- ${SERVERS[index]}" ":${API_PORT}"
+  echo "- ${s}:${API_PORT}"
 done
